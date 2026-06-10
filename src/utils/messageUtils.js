2bc0a53c3d4a53bac83617373eeddd87323011bc
gameforming <96012344@student.theresialyceum.nl>
2026-06-10 09:32:26 +0200

    Add dedupeMessages function to filter unique messages

---
+++ src/utils/messageUtils.js
@@ -0,0 +1,12 @@
+export function dedupeMessages(messages) {
+  const seen = new Set();
+
+  return messages.filter((message) => {
+    if (seen.has(message.id)) {
+      return false;
+    }
+
+    seen.add(message.id);
+    return true;
+  });
+}
