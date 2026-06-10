d51f7e448562ca871be8f4a4e1f4db2007eb9c44
gameforming <96012344@student.theresialyceum.nl>
2026-06-10 09:33:50 +0200

    Create useChatStore.js

---
+++ src/store/useChatStore.js
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+
+  setMessages: (messages) =>
+    set({ messages }),
+
+  addMessage: (message) =>
+    set((state) => ({
+      messages: [...state.messages, message]
+    })),
+
+  clearMessages: () =>
+    set({
+      messages: []
+    })
+}));
